1401aa84b8e3279307a50f1f1544e3cdb19830a1
Silk <sanranyywitch15@gmail.com>
2026-09-22 18:33:48 +0700

    ลบcomputeBuffer เพิ่มส่วนหัวไฟล์

--- module_d/displayHexDump.asm
+++ module_d/displayHexDump.asm
@@ -1,5 +1,25 @@
+.386
+.model flat, stdcall
+INCLUDE C:\Irvine\Irvine32.inc
+INCLUDELIB C:\Irvine\Irvine32.lib
+INCLUDELIB C:\Irvine\Kernel32.lib
+INCLUDELIB C:\Irvine\User32.lib
+
+.data
+    format BYTE "[Address]  00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F  |  ASCII ", 0
+    line BYTE "-------------------------------------------------------------------------", 0
+    space BYTE " "
+    wall BYTE "|"
+
+.code
 DisplayHexDump PROC
     pushad
+
+    ;Print format
+    mov edx, OFFSET format
+    call WriteString
+    mov edx, OFFSET line
+    call WriteString
 
     ; ESI = pointer
     ; ECX = length
@@ -36,7 +56,7 @@ DumpLoop:
     mov dl, '.'
     call WriteChar
 
-Continue:
+    Continue:
     ; ไป byte ถัดไป
     inc esi
     dec ecx
@@ -47,3 +67,31 @@ DumpDone:
     popad
     ret
 DisplayHexDump ENDP
+
+ComputeBufferStats PROC
+    pushad
+
+    ; ESI = input buffer
+    ; ECX = buffer length
+
+    mov esi, edx
+    mov ecx, eax
+
+CountLoop:
+    cmp ecx, 0
+    je CountDone
+
+    xor ebx, ebx
+    mov bl, [esi]          ; BL = byte ที่อ่านมา
+
+    inc histogram[ebx*4]   ; histogram[byte]++
+
+    inc esi
+    dec ecx
+
+    jmp CountLoop
+
+CountDone:
+    popad
+    ret
+ComputeBufferStats ENDP
