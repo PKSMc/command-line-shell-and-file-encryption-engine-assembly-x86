be388fe7d7c8fd1136f31fa3afb21bdac71ab347
Silk <sanranyywitch15@gmail.com>
2026-09-22 09:52:48 +0700

    เพิ่มFloder CombineAll และแก้ไข Readme.txt เขียนชื่อและหน้าที่

---
+++ CombineAll/Combine.asm
@@ -0,0 +1,20 @@
+TITLE Classical DES 
+
+;เพิ่ม Include ตรงนี้
+INCLUDE C:\Irvine\Irvine32.inc
+INCLUDELIB C:\Irvine\Irvine32.lib
+INCLUDELIB C:\Irvine\Kernel32.lib
+INCLUDELIB C:\Irvine\User32.lib  ;อันนี้เพิ่มไว้ก่อน
+
+;เพิ่มข้อมูลตรงนี้ถ้ามี
+.data
+
+;เขียนโค้ด/Procedure ตรงนี้
+.code
+
+main PROC
+    ;call Procudure ตรงนี้
+
+
+main ENDP
+END main
--- readme.txt
+++ readme.txt
@@ -1,1 +1,4 @@
-Test
+Module A : 
+Module B : 68010980 นาย วรนณ มีทอง
+Module C :
+Module D : 68011047 นาย ศรัณย์ญวิชญ์ เปรมจิต
